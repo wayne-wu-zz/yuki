9c62bbbb6028455f028dda3ca253c6fb344871fe
misspia <pia_leung@hotmail.com>
2018-07-14 00:19:40 -0400

    render test cube, add orbit controls

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 import '../css/main.css';
+import * as THREE from 'three';
 import SceneManager from './sceneManager.js';
 
 import vert from '../shaders/snow.vert';
 import frag from '../shaders/snow.frag';
 
 const canvas = document.getElementById('canvas');
-const { camera, renderer, scene } = new SceneManager(canvas);
+const app = new SceneManager(canvas);
+
+// app.addOrbitControls();
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial({ color: 0xff000f, wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+app.scene.add( cube );
+
+app.animate(() => {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+});
--- js/sceneManager.js
+++ js/sceneManager.js
@@ -14,15 +14,22 @@ class SceneManager {
       this.init();
   }
   init() {
-    this.initRenderer();
-    this.initCamera();
     this.initScene();
+    this.initCamera();
+    this.initRenderer();
+    // this.addOrbitControls()
+    this.controls = new OrbitController(this.camera, this.renderer.domElement);
 
     // inital resize
     this.resize();
-
-    // window event listeners
+    //
+    // // window event listeners
     window.addEventListener('resize', () => this.resize());
+  }
+  animate(callback) {
+    callback();
+    requestAnimationFrame( () => this.animate(callback) );
+    this.renderer.render( this.scene, this.camera );
   }
   initRenderer() {
     this.renderer = new THREE.WebGLRenderer({
@@ -39,8 +46,8 @@ class SceneManager {
   }
   initCamera() {
     const aspectRatio = window.innerWidth / window.innerHeight;
-    this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.01, 100);
-    this.camera.position.set(0, 1, -3);
+    this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    this.camera.position.set(0, 1, 5);
   }
   initScene() {
     this.scene = new THREE.Scene();
@@ -51,10 +58,8 @@ class SceneManager {
   }
   resize() {
     const { innerHeight: height, innerWidth: width } = window;
-
+    this.updateProjectionMatrix(width, height);
     this.renderer.setSize(width, height);
-    this.updateProjectionMatrix();
-    this.camera.updateProjectionMatrix(width, height);
   }
   updateProjectionMatrix(width, height) {
     this.camera.aspect = width / height;
